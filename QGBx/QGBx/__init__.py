41aee55ede43550a7120fc2afe91996c4296bec9
AshrafBoussahi <55757978+AshrafBoussahi@users.noreply.github.com>
2025-08-10 17:38:49 +0100

    Delete QGBx directory

--- QGBx/QGBx/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base.generator import Generator
-from .base.device import Device
-from .base.distribution import Distribution
-from .base.runner import Runner
-from .base.visualiser import Visualiser
--- QGBx/QGBx/distibutions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .gaussia n import Gaussian
-from.hadamard_QW import HadamardQW
-from .exponential import Exponential
